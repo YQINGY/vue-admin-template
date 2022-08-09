225f4414afc27523eee988f5d2b251c0f7880e5a
YQINGY <57324808+YQINGY@users.noreply.github.com>
2022-08-09 22:10:27 +0800

    更新样式

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,11 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
   text-align: center;
   color: #2c3e50;
 }
