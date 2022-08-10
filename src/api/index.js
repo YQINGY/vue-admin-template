42bdc28355fb0e3e8f4def4a4a22c837f340f56c
YQINGY <57324808+YQINGY@users.noreply.github.com>
2022-08-10 19:06:22 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,13 +3,6 @@
  * @Date: 2022-07-23 16:24:07
  * @LastEditTime: 2022-08-06 12:23:58
  */
-import request from '@/utils/request'
+import { post, get } from '@/utils/request'
 
-
-export function userVerify(params) {
-    return request.get('/user/verify', params)
-}
-
-export function userLogin(params) {
-    return request.post('/user/login', params)
-}
+export const test = params => get('/api/test', params);
