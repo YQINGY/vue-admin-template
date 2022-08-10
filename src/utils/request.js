29c7ab495da5baac93f6a0194dc8e992f55dcb96
YQINGY <57324808+YQINGY@users.noreply.github.com>
2022-08-10 19:05:20 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -86,5 +86,37 @@ service.interceptors.response.use(response => {
     }
     return Promise.reject(error);
 })
+/**
+ * get方法，对应get请求
+ * @param {String} url [请求的url地址]
+ * @param {Object} params [请求时携带的参数]
+ */
+export function get(url, params) {
+    return new Promise((resolve, reject) => {
+        service.get(url, {
+            params: params
+        }).then(res => {
+            resolve(res);
+        }).catch(err => {
+            reject(err)
+        })
+    });
+}
 
-export default service
+/** 
+ * post方法，对应post请求 
+ * @param {String} url [请求的url地址] 
+ * @param {Object} params [请求时携带的参数] 
+ */
+export function post(url, params) {
+    console.log(url, params)
+    return new Promise((resolve, reject) => {
+        service.post(url, params)
+            .then(res => {
+                resolve(res);
+            })
+            .catch(err => {
+                reject(err)
+            })
+    });
+}
